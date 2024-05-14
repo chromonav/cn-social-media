98939a87da65ac1b1224363b9d31c0a9750f56f6
RahulSarkar <bluephoenix00995@gmail.com>
2024-05-14 12:49:48 +0530

    added popUp

--- cn_social_media/content_inspiration/doctype/content_inspiration/content_inspiration.js
+++ cn_social_media/content_inspiration/doctype/content_inspiration/content_inspiration.js
@@ -6,3 +6,27 @@
 
 // 	},
 // });
+
+
+frappe.ui.form.on('Content Inspiration', {
+    refresh: function(frm) {
+        frm.fields_dict['accept'].$input.click(function() {
+            frappe.prompt([
+                {'fieldname': 'field1', 'fieldtype': 'Data', 'label': 'Hook title'},
+                {'fieldname': 'field2', 'fieldtype': 'Select', 'label': 'Social Media Account', 'reqd': 1},
+                {'fieldname': 'field1', 'fieldtype': 'Text', 'label': 'Notes'},
+            ],
+            function(values){
+                frappe.msgprint('You entered ' + values.field1 + ' and ' + values.field2);
+            },
+            'New Post',
+            'Create Post'
+            )
+        });
+
+        
+        frm.fields_dict['reject'].$input.click(function() {
+            frappe.msgprint('You clicked on Reject');
+        });
+    }
+});
