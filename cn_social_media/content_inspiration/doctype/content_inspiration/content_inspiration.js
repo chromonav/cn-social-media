1441f76ba2950e3b3acbf4e3ee28ad571944ec94
RahulSarkar <bluephoenix00995@gmail.com>
2024-05-14 14:18:21 +0530

    added popUp and record the data

--- cn_social_media/content_inspiration/doctype/content_inspiration/content_inspiration.js
+++ cn_social_media/content_inspiration/doctype/content_inspiration/content_inspiration.js
@@ -12,21 +12,42 @@ frappe.ui.form.on('Content Inspiration', {
     refresh: function(frm) {
         frm.fields_dict['accept'].$input.click(function() {
             frappe.prompt([
-                {'fieldname': 'field1', 'fieldtype': 'Data', 'label': 'Hook title'},
-                {'fieldname': 'field2', 'fieldtype': 'Select', 'label': 'Social Media Account', 'reqd': 1},
-                {'fieldname': 'field1', 'fieldtype': 'Text', 'label': 'Notes'},
+                {'fieldname': 'hook_title', 'fieldtype': 'Data', 'label': 'Hook title'},
+                {'fieldname': 'social_media_account', 'fieldtype': 'Select', 'label': 'Social Media Account', 'reqd': 1, 'options': ['Sample', 'test1', 'test2']},
+                {'fieldname': 'notes', 'fieldtype': 'Text', 'label': 'Notes'},
             ],
             function(values){
-                frappe.msgprint('You entered ' + values.field1 + ' and ' + values.field2);
+                frappe.msgprint('You entered ' + values.hook_title + ', ' + values.social_media_account + ', and ' + values.notes);
+
+                // Create a new document
+                var doc = frappe.model.get_new_doc('Social Media Post');
+
+                // Set the fields
+                doc.social_media_title = values.hook_title;
+                doc.social_media_account = values.social_media_account;
+                doc.content_inspiration_synopsis = values.notes;
+
+                // Insert the document
+                frappe.call({
+                    method: 'frappe.client.insert',
+                    args: {
+                        doc: doc
+                    },
+                    callback: function(response) {
+                        if (!response.exc) {
+                            frappe.msgprint('Record inserted successfully into Social Media Post');
+                        }
+                    }
+                });
             },
             'New Post',
             'Create Post'
             )
         });
 
-        
         frm.fields_dict['reject'].$input.click(function() {
             frappe.msgprint('You clicked on Reject');
         });
     }
 });
+
